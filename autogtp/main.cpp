0ab4ad1cb9ee5458da0e70092c1f2f6a4c013a19
nanzi <nanzhongjie@gmail.com>
2023-04-18 10:58:05 -0400

    Update main.cpp

--- autogtp/main.cpp
+++ autogtp/main.cpp
@@ -38,6 +38,16 @@
 #include "Game.h"
 #include "Management.h"
 #include "Console.h"
+#include <QtGlobal>
+#include <QString>
+
+#if QT_VERSION < QT_VERSION_CHECK(5, 14, 0)
+namespace Qt
+{
+    static auto endl = ::endl;
+    static auto SkipEmptyParts = QString::SkipEmptyParts;
+}
+#endif
 
 int main(int argc, char *argv[]) {
     QCoreApplication app(argc, argv);
